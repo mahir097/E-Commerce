406d62552a25881077bc3a8a1548ba2a52131468
Mahir <mahir.demir097@gmail.com>
2024-02-24 16:33:37 +0300

    Models

--- shopapp.webui/Controllers/ProductController.cs
+++ shopapp.webui/Controllers/ProductController.cs
@@ -3,8 +3,9 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
+using shopapp.webui.Models;
 
-    //localhost:5000/product
+//localhost:5000/product
 namespace shopapp.webui.Controllers
 {
     public class ProductController:Controller
@@ -23,7 +24,16 @@ namespace shopapp.webui.Controllers
         //localhost:5000/product/details
         public IActionResult Details(int id)
         {
-            return View();
+            // ViewBag.Name = "Samsung S6";
+            // ViewBag.Price = 2000;
+            // ViewBag.Description = "İyi Telefon";
+
+            var p = new Product();
+            p.Name = "Samsung S6";
+            p.Price = "2000";
+            p.Description = "İyi Telefon";
+
+            return View(p);
         }
     }
 }
---
+++ shopapp.webui/Models/Product.cs
@@ -0,0 +1,14 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace shopapp.webui.Models
+{
+    public class Product
+    {
+        public string Name { get; set; }
+        public string Price { get; set; }
+        public string Description { get; set; }
+    }
+}
